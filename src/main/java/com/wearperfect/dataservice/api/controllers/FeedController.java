298dac049793a0206c0b8596b9a9136f51ffc321
WearPerfect <70898247+wearperfectnow@users.noreply.github.com>
2022-05-06 19:44:31 +0530

    initial cache config

--- src/main/java/com/wearperfect/dataservice/api/controllers/FeedController.java
+++ src/main/java/com/wearperfect/dataservice/api/controllers/FeedController.java
@@ -3,6 +3,7 @@ package com.wearperfect.dataservice.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class FeedController {
 		return feedService.getFeed();
 	}
 	
+	@Cacheable(value="feed", key = "#userId")
 	@GetMapping(path = "/users/{userId}/feed")
 	List<PostDetailsDTO> getFeedBUserId(@PathVariable(name = "userId", required = true) Long userId){
 		return feedService.getFeedByUserId(userId);
--- src/main/java/com/wearperfect/dataservice/api/controllers/UserController.java
+++ src/main/java/com/wearperfect/dataservice/api/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.wearperfect.dataservice.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
